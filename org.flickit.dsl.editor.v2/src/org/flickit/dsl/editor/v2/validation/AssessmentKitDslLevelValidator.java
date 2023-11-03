530c4bd79df14672c41f836a99e2fd87906fc066
mahdirezazadeh <riza.rs2000@gmail.com>
2023-11-03 21:51:56 +0330

    DE-6:feature:Refactor

--- org.flickit.dsl.editor.v2/src/org/flickit/dsl/editor/v2/validation/AssessmentKitDslLevelValidator.java
+++ org.flickit.dsl.editor.v2/src/org/flickit/dsl/editor/v2/validation/AssessmentKitDslLevelValidator.java
@@ -4,6 +4,7 @@ import org.eclipse.xtext.validation.Check;
 import org.flickit.dsl.editor.v2.assessmentKitDsl.AssessmentKitDslPackage;
 import org.flickit.dsl.editor.v2.assessmentKitDsl.Competence;
 import org.flickit.dsl.editor.v2.assessmentKitDsl.CompetenceValue;
+import org.flickit.dsl.editor.v2.assessmentKitDsl.Level;
 
 public class AssessmentKitDslLevelValidator extends AbstractAssessmentKitDslValidator {
 
@@ -11,15 +12,29 @@ public class AssessmentKitDslLevelValidator extends AbstractAssessmentKitDslVali
 	public static final int COMPETENCE_VALUE_MAX = 100;
 
 	@Check
-	public void checkCompetenceValueNotOutOfRange(Competence competence) {
-		for(CompetenceValue value:competence.getCompetenceValues()) {
-			if (value.getValue() < COMPETENCE_VALUE_MIN)
-				error(String.format("'Competence value' may not be less than %d!", COMPETENCE_VALUE_MIN),
-						AssessmentKitDslPackage.Literals.COMPETENCE__COMPETENCE_VALUES);
-	
-			if (value.getValue() > COMPETENCE_VALUE_MAX)
-				error(String.format("'Competence value' may not be more than %d!", COMPETENCE_VALUE_MAX),
-						AssessmentKitDslPackage.Literals.COMPETENCE__COMPETENCE_VALUES);
+	public void checkCompetenceValueNotOutOfRange(Level level) {
+		if (hasCompetenceValueLessThanMin(level)) {
+			reportError("Competence value may not be less than " + COMPETENCE_VALUE_MIN, AssessmentKitDslPackage.Literals.LEVEL__COMPETENCE);
 		}
+
+		if (hasCompetenceValueMoreThanMax(level)) {
+			reportError("Competence value may not be more than " + COMPETENCE_VALUE_MAX, AssessmentKitDslPackage.Literals.LEVEL__COMPETENCE);
+		}
+	}
+
+	private boolean hasCompetenceValueLessThanMin(Level level) {
+		return level.getCompetence().stream()
+				.anyMatch(c -> c.getCompetenceValues().stream()
+						.anyMatch(v -> v.getValue() < COMPETENCE_VALUE_MIN));
+	}
+
+	private boolean hasCompetenceValueMoreThanMax(Level level) {
+		return level.getCompetence().stream()
+				.anyMatch(c -> c.getCompetenceValues().stream()
+						.anyMatch(v -> v.getValue() > COMPETENCE_VALUE_MAX));
+	}
+
+	private void reportError(String message, EStructuralFeature feature) {
+		error(message, feature);
 	}
 }
