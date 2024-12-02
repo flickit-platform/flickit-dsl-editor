6c45c09090e3717ed3939897899d766b8c8b6a64
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-12-02 15:55:17 +0330

    DE-18 Add defining answerRange to grammar rules
    Add changes related to adding of answerRange

--- org.flickit.dsl.editor.v2/src/org/flickit/dsl/editor/v2/validation/AssessmentKitDslQuestionValidator.java
+++ org.flickit.dsl.editor.v2/src/org/flickit/dsl/editor/v2/validation/AssessmentKitDslQuestionValidator.java
@@ -17,19 +17,4 @@ public class AssessmentKitDslQuestionValidator extends AbstractAssessmentKitDslV
 				error("'Hint' may not be empty!",
 						AssessmentKitDslPackage.Literals.QUESTION__HINT);
 	}
-
-	@Check
-	public void checKQuestionAnswersNumber(Question question) {
-		Set<Integer> valuesSizes = question.getAffectsAttribute().stream()
-				.flatMap(a -> a.getOnLevel().stream().map(l ->
-				{
-					return l.getValues().size();
-				})).collect(Collectors.toSet());
-
-		valuesSizes.remove(0);
-
-		if (valuesSizes.size() > 1 || (valuesSizes.size() == 1 && !valuesSizes.contains(question.getOptions().size())))
-			error("Affects attribute values should have same size with options of question!",
-					AssessmentKitDslPackage.Literals.QUESTION__AFFECTS_ATTRIBUTE);
-	}
 }
