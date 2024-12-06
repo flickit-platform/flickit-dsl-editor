a131d91b05a6499d7bf0186b19ea5bceb18df0e0
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-12-06 15:04:04 +0330

    Add question options/range validation

--- org.flickit.dsl.editor.v2/src/org/flickit/dsl/editor/v2/validation/AssessmentKitDslQuestionValidator.java
+++ org.flickit.dsl.editor.v2/src/org/flickit/dsl/editor/v2/validation/AssessmentKitDslQuestionValidator.java
@@ -11,19 +11,25 @@ public class AssessmentKitDslQuestionValidator extends AbstractAssessmentKitDslV
 	public void checkHintNotEmpty(Question question) {
 		if (question.getHint() != null)
 			if (Strings.isEmpty(question.getHint()))
-				error("'Hint' may not be empty!",
-						AssessmentKitDslPackage.Literals.QUESTION__HINT);
+				error("'Hint' may not be empty!", AssessmentKitDslPackage.Literals.QUESTION__HINT);
 	}
-	
+
 	@Check
 	public void checkQuestionOptionsNumber(Question question) {
 		if (question.getOptions() != null && !question.getOptions().isEmpty()) {
 			var optionsSize = question.getOptions().size();
 			var valuesSize = question.getValues().size();
-			
+
 			if (optionsSize != valuesSize)
-				error("Option values should have same size with options of question!",
+				error("Option values size should match the question options size!",
 						AssessmentKitDslPackage.Literals.QUESTION__OPTIONS);
 		}
+	}
+
+	@Check
+	public void checkQuestionOptionsOrAnswerRangeExistence(Question question) {
+		if ((question.getOptions() == null || question.getOptions().isEmpty()) && question.getAnswerRange() == null)
+			error("Question should have options/answerRange but not both!",
+					AssessmentKitDslPackage.Literals.QUESTION__OPTIONS);
 	}
 }
